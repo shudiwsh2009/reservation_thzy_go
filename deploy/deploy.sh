341a816d40957896647d1d44d422459e2d77c2a3
Shudi Wang <shudiwsh2009@gmail.com>
2016-03-01 15:48:55 +0800

    update deploy files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,6 @@
 echo "#update git"
 go get -u github.com/shudiwsh2009/reservation_thzy_go
 go get -u github.com/shudiwsh2009/reservation_thzy_go_reminder
-go get -u github.com/shudiwsh2009/reservation_thzy_go_timetable
 
 echo "#deploy website"
 supervisorctl stop reservation_thzy_go
@@ -13,9 +12,6 @@ supervisorctl start reservation_thzy_go
 
 echo "#deploy reminder"
 cd $GOPATH/src/github.com/shudiwsh2009/reservation_thzy_go_reminder
-go build
-echo "#deploy timetable"
-cd $GOPATH/src/github.com/shudiwsh2009/reservation_thzy_go_timetable
 go build
 
 echo "restart cron"
